7a4d2f1ce69982f8aeb55607964426336c766cea
alonsovargas90 <alonsovargas90@gmail.com>
2025-05-20 13:30:48 -0600

    add refactor for the translations

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,9 +3,32 @@ import { locales, defaultLocale } from '@/i18n/i18n' // si lo tienes definido ah
 
 export default getRequestConfig(async ({ locale }) => {
   const safeLocale = locale && locales.includes(locale) ? locale : defaultLocale;
+ const files = [
+    'hero',
+    'footer',
+    'nav',
+    'ourTeam',
+    'ourServices',
+    'reviews',
+    'servicePages',
+    'aboutPage',
+    'heroContact',
+    'invisalignBanner',
+    'topPriority',
+    'userCase'
+  ];
+
+  const messages = Object.assign(
+    {},
+    ...(await Promise.all(
+      files.map(async (file) =>
+        (await import(`../messages/${locale}/${file}.json`)).default
+      )
+    ))
+  );
 
   return {
-    messages: (await import(`../messages/${safeLocale}.json`)).default,
+    messages,
     locale: safeLocale
   }
 })
