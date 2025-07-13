816ddc200789d90ecdfcbe6773a8adc9a59c5262
alonsovargas90 <alonsovargas90@gmail.com>
2025-07-13 17:56:27 -0600

    Add latest svgs and remove coming soon landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,62 +1,15 @@
-// // app/page.tsx
-// 'use client'
-
-// import { useRouter } from 'next/navigation'
-// import { useEffect } from 'react'
-
-// export default function HomeRedirect() {
-//   const router = useRouter()
-
-//   useEffect(() => {
-//     router.replace('/en')
-//   }, [router])
-
-//   return null
-// }
+// app/page.tsx
 'use client'
 
-import { Box, Typography } from '@mui/material'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-export default function Home() {
+export default function HomeRedirect() {
+  const router = useRouter()
 
-  return (
-    <Box
-      sx={{
-        minHeight: '100vh',
-        width: '100%',
-        backgroundImage: 'url(/hero.png)',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        position: 'relative',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#fff',
-      }}
-    >
-      {/* Overlay */}
-      <Box
-        sx={{
-          position: 'absolute',
-          inset: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent black
-          zIndex: 1,
-        }}
-      />
+  useEffect(() => {
+    router.replace('/es')
+  }, [router])
 
-      {/* Centered Content */}
-      <Typography
-        variant="h2"
-        sx={{
-          position: 'relative',
-          zIndex: 2,
-          textAlign: 'center',
-          fontWeight: 700,
-          px: 2,
-        }}
-      >
-        {'Coming Soon'}
-      </Typography>
-    </Box>
-  )
+  return null
 }
